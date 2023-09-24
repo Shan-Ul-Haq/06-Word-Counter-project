afdc1504f166777518d6b81851192a0db53425d7
Shan-Ul-Haq <shanulhaq84@gmail.com>
2023-09-24 15:57:02 +0500

    Initial commit

---
+++ wordcounter.ts
@@ -0,0 +1,30 @@
+import chalk from "chalk";
+import inquirer from "inquirer";
+import chalkAnimation from "chalk-animation"
+
+const sleep = () => {
+    return new Promise((res) => {
+        setTimeout(res, 3000)
+    })
+}
+async function welcome() {
+    let rainbowTitle = chalkAnimation.rainbow("Welcome to Typescript Word Counter Made By Shan");
+    await sleep();
+    rainbowTitle.stop();
+}
+await welcome()
+
+
+const answer: {
+    sentence: string
+}
+ = await inquirer.prompt([
+    {
+        type: "input",
+        name: "sentence",
+        message: "Write a sentence to count the word:"
+    }])
+
+    const words = answer.sentence.split(" ")
+    console.log(`Please count the words ${words.length}`);
+    
